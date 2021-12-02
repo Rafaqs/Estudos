c52dfcbfbbef960c532b16163bb174512feebd3e
Rafaqs <queiros.rafael@gmail.com>
2021-12-01 23:12:26 -0300

    Add project files.

---
+++ ER_S09/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ER_S09.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department() {}
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ ER_S09/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace ER_S09.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
